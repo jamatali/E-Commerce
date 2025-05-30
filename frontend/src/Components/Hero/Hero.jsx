6dcbb91a1c07d4eb3d8cc893bd6b801d70b624af
jamatali <engr.jamatali@gmail.com>
2025-05-30 17:57:21 +0500

    fixes

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,28 +1,28 @@
-import React from "react";
 import "./Hero.css";
 import hand_icon from "../Assets/hand_icon.png";
-import arrow_icom from "../Assets/arrow.png";
-import hero_img from "../Assets/hero_image.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
+
 export const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARRIVALS</h2>
+        <h2>NEW ARRIVALS ONLY</h2>
         <div>
           <div className="hero-hand-icon">
             <p>new</p>
-            <img src={hand_icon} alt="hero hand icon" />
+            <img src={hand_icon} alt="" />
           </div>
           <p>collections</p>
           <p>for everyone</p>
         </div>
         <div className="hero-latest-btn">
           <div>Latest Collection</div>
-          <img src={arrow_icom} alt="Arrow Icon" />
+          <img src={arrow_icon} alt="" />
         </div>
       </div>
       <div className="hero-right">
-        <img src={hero_img} alt="Hero Banner" />
+        <img src={hero_image} alt="" />
       </div>
     </div>
   );
